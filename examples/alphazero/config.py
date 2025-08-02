6ff1e219c8cfe0e97ec6051407f144d0091f9547
Hua Yu <hyu2000@gmail.com>
2025-08-02 09:30:42 -0400

    - optimizer: weight-decay, lr_schedule - filter out terminal/truncated samples

--- examples/alphazero/config.py
+++ examples/alphazero/config.py
@@ -7,7 +7,7 @@ class Config(BaseModel):
     seed: int = 0
     max_num_iters: int = 400
     # network params
-    num_channels: int = 64
+    num_channels: int = 64  # 128
     num_layers: int = 6
     resnet_v2: bool = True
     # selfplay params
@@ -17,6 +17,8 @@ class Config(BaseModel):
     # training params
     training_batch_size: int = 4096   # #samples per batch
     learning_rate: float = 0.001
+    lr_decay_steps: int = 4096  # halve LR every #steps
+    weight_decay: float = 1e-4
     # eval params
     eval_interval: int = 5
     eval_batch_size: int = 128  # #games
