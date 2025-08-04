03848a76f4d07ee71aec157a2eba012f8994093e
Hua Yu <hyu2000@gmail.com>
2025-08-04 09:31:08 -0400

    minor, still general case to work with both train/eval -- I'm thinking of changing model_apply to be partial(model_apply, model_params)

--- examples/alphazero/mctx_search.py
+++ examples/alphazero/mctx_search.py
@@ -5,51 +5,57 @@ import mctx
 import pgx
 
 
-def recurrent_fn(model, rng_key: jnp.ndarray, action: jnp.ndarray, state: pgx.State):
-    """ It'll be best to encapsulate forward.apply in model; env.step in state.
-    Seems they can be pytree.
-    state/action: array
+def make_recurrent_fn(forward_apply, env_step):
+    """ forward_apply is not bound w/ params
     """
-    # model: params
-    # state: embedding
-    del rng_key
+
+    def recurrent_fn(model, rng_key: jnp.ndarray, action: jnp.ndarray, state: pgx.State):
+        """
+        seems model cannot be Any (but a0jax uses eqx.Module); state can be Any
+        state/action: array
+        """
+        # model: params
+        # state: embedding
+        del rng_key
+        model_params, model_state = model
+
+        current_player = state.current_player
+        state = jax.vmap(env_step)(state, action)
+
+        (logits, value), _ = forward_apply(model_params, model_state, state.observation, is_eval=True)
+        # mask invalid actions
+        logits = logits - jnp.max(logits, axis=-1, keepdims=True)
+        logits = jnp.where(state.legal_action_mask, logits, jnp.finfo(logits.dtype).min)
+
+        reward = state.rewards[jnp.arange(state.rewards.shape[0]), current_player]
+        value = jnp.where(state.terminated, 0.0, value)
+        discount = -1.0 * jnp.ones_like(value)
+        discount = jnp.where(state.terminated, 0.0, discount)
+
+        recurrent_fn_output = mctx.RecurrentFnOutput(
+            reward=reward,
+            discount=discount,
+            prior_logits=logits,
+            value=value,
+        )
+        return recurrent_fn_output, state
+
+    return recurrent_fn
+
+
+def improve_policy_with_mcts(forward_apply, recurrent_fn, model, state, rng_key, num_simulations: int):
     model_params, model_state = model
-
-    current_player = state.current_player
-    state = jax.vmap(env.step)(state, action)
-
-    (logits, value), _ = forward.apply(model_params, model_state, state.observation, is_eval=True)
-    # mask invalid actions
-    logits = logits - jnp.max(logits, axis=-1, keepdims=True)
-    logits = jnp.where(state.legal_action_mask, logits, jnp.finfo(logits.dtype).min)
-
-    reward = state.rewards[jnp.arange(state.rewards.shape[0]), current_player]
-    value = jnp.where(state.terminated, 0.0, value)
-    discount = -1.0 * jnp.ones_like(value)
-    discount = jnp.where(state.terminated, 0.0, discount)
-
-    recurrent_fn_output = mctx.RecurrentFnOutput(
-        reward=reward,
-        discount=discount,
-        prior_logits=logits,
-        value=value,
-    )
-    return recurrent_fn_output, state
-
-
-def improve_policy_with_mcts(forward_apply_fn, env_step_fn, model, state, num_simulations: int):
-    model_params, model_state = model
-    (logits, value), _ = forward.apply(
+    (logits, value), _ = forward_apply(
         model_params, model_state, state.observation, is_eval=True
     )
     root = mctx.RootFnOutput(prior_logits=logits, value=value, embedding=state)
 
     policy_output = mctx.gumbel_muzero_policy(
         params=model,
-        rng_key=key1,
+        rng_key=rng_key,
         root=root,
         recurrent_fn=recurrent_fn,
-        num_simulations=config.num_simulations,
+        num_simulations=num_simulations,
         invalid_actions=~state.legal_action_mask,
         qtransform=mctx.qtransform_completed_by_mix_value,
         gumbel_scale=1.0,
