45742785497df4b77ca69c17337b8ca5b465fc9c
Hua Yu <hyu2000@gmail.com>
2025-09-14 19:35:41 -0400

    minor

--- tests/test_go_baseline.py
+++ tests/test_go_baseline.py
@@ -322,7 +322,7 @@ num_simulations=64: Total 64 games, win-rate= 0.78125
 
     num_simulations = 32
     num_eval_games = 64
-    batch_forward2, _, _ = load_go5_checkpoint_eqx('go_5x5C2_250906-125418/000075.ckpt')
+    batch_forward2, _, _ = load_go5_checkpoint_eqx('go_5x5C2_250909-160146/000050.ckpt')
     batch_policy2 = batch_forward_to_policy(batch_forward2)
     batch_forward_mcts2 = mctx_search.batch_fwd_mcts_to_policy(
         mctx_search.get_batch_fwd_mcts(batch_forward2, env.step, num_simulations=num_simulations))
